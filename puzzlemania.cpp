6db86d717678b77e720b19d3c214fba3e9d3e936
Wichitchai <60010933@kmitl.ac.th>
2019-08-30 02:45:09 +0700

    get position function

--- puzzlemania.cpp
+++ puzzlemania.cpp
@@ -4,33 +4,41 @@
 /* MAPP:
 7 11
 ###########
-#T##......#
-#.#.#..####
+#.##......#
+#T#.#..####
 #....B...S#
 #.######..#
 #.........#
 ###########
 */
 using namespace std;
-
+ 
+typedef struct po{
+	int x,y;
+}position;
+position goal,now,temp;
 
 char mapp[MAPHEIGHT][MAPLENGTH];
-//void getIn();
-//void showMap();
-
+void getIn();
+void showMap();
+void getPosition();
+void DFS();
 int main(){
-	cout << "welcome back CXz." << endl;
+//	cout << "welcome back CXz." << endl;
 	getIn();
 	showMap();
+	getPosition();
 	return 0;
 }
 /////////////////////////////////////
 //	functionZone
 /////////////////////////////////////
+
 void getIn(){
 	for(int i = 0; i < MAPHEIGHT ; i++){
 		scanf(" %[^\n]s",mapp[i]);
 	}
+//	mapp = "############T##......##.#.#..#####....B...S##.######..##.........############";	
 }
 void showMap(){
 	cout << "--------------------------\n";
@@ -42,6 +50,23 @@ void showMap(){
 	}
 	cout << "--------------------------\n";
 }
-
+void getPosition(){
+	cout << "-------------USE getPosition\n";
+	for(int i = 0 ; i < MAPHEIGHT;i++){
+		for(int j = 0 ; j < MAPLENGTH ; j++){
+			if(mapp[i][j]=='T') goal.x = i , goal.y = j;
+			else if(mapp[i][j]=='S') now.x = i, now.y = j;
+		}
+	}
+	cout << "goal :\n\tx: " << goal.x << endl;
+	cout << "\ty: " << goal.y << endl;
+	cout << "now start:\n\tx: "<< now.x << endl;
+	cout << "\ty: " << now.y << endl;
+	cout << "-------------END getPosition\n";
+	
+}
+void DFS(){
+	
+}
 
 
