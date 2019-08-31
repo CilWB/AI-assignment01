37a69f29742ecbfc2279c9fd417e60bb17a02961
Wichitchai <60010933@kmitl.ac.th>
2019-08-31 20:28:34 +0700

    more test-cases and prototype version

--- puzzlemania.cpp
+++ puzzlemania.cpp
@@ -7,6 +7,7 @@ using namespace std;
 typedef struct po{
 	int x,y,step;
 	char act[100];
+	char stage[20][20];
 }position;
 position goal,now,start;
 queue<position>que;
@@ -16,18 +17,24 @@ int direction[4][2] = {{0,-1},{1,0},{0,1},{-1,0}};
 // 1-> s
 // 2-> e
 // 3-> n
-char ccc[5] = "wsen";
-
+char ccc[9] = "wsenWSEN";
+char zzz[9] = "enws";
 char mapp[MAPHEIGHT][MAPLENGTH];
+char went[MAPHEIGHT][MAPLENGTH];
 void getIn();
 void showMap();
 void getPosition();
+void showWent();
+void showStage(position);
+bool inBound();
 int BFS(position,position);
+
 int main(){
 //	cout << "welcome back CXz." << endl;
 	getIn();
 	showMap();
 	getPosition();
+//	showStage(start);
 	cout << "BFS " << BFS(start,goal);
 	return 0;
 }
@@ -62,10 +69,11 @@ void showMap(){
 }
 void getPosition(){
 	cout << "-------------USE getPosition\n";
-	for(int i = 0 ; i < MAPHEIGHT;i++){
-		for(int j = 0 ; j < MAPLENGTH ; j++){
+	for(int i = 0 ; i < r;i++){
+		for(int j = 0 ; j < c ; j++){
 			if(mapp[i][j]=='T') goal.x = i , goal.y = j;
 			else if(mapp[i][j]=='S') start.x = i, start.y = j;
+			start.stage[i][j] = mapp[i][j];
 		}
 	}
 	start.step = 0;
@@ -76,16 +84,43 @@ void getPosition(){
 	cout << "-------------END getPosition\n";
 	
 }
+void showWent(){
+	cout << "--------------------------showWent\n";
+	for(int x = 0 ; x < r;x++){
+		for(int j = 0 ; j < c ; j++){
+			cout << char(went[x][j]+'0');
+		}
+		cout << endl;
+	}
+	cout << "--------------------------\n";	
+}
+void showStage(position x){
+	
+	cout << "--------------------------showStage\n";
+	for(int i = 0 ; i < r;i++){
+		for(int j = 0 ; j < c ; j++){
+			cout << x.stage[i][j];
+		}
+		cout << endl;
+	}
+	cout << "--------------------------\n";	
+}
+bool inBound(){
+	return true;
+}
+bool checkBox(position n){
+	
+}
 int BFS(position nnow,position goal){
 //	cout << "welcome to BFS\n";
 	que.push(nnow); // add a start state
 	if(start.x == goal.x && start.y == goal.y){
 		return 0;
 	}
-	char went[MAPHEIGHT][MAPLENGTH]; 
-	for(int i = 0 ; i< MAPHEIGHT;i++)
-		for(int j = 0 ; j <MAPLENGTH;j++)
-			went[i][j] = 0;
+	 
+//	for(int i = 0 ; i< r;i++)
+//		for(int j = 0 ; j <c;j++)
+//			went[i][j] = 0;
 	while(1){
 		if(que.empty()){
 			return -1;	
@@ -93,63 +128,124 @@ int BFS(position nnow,position goal){
 		else{
 			position now = que.front();
 			que.pop();
-			went[now.x][now.y] = 1;
+//			went[now.x][now.y] = 1;
 			
 			for(int i = 0 ; i < 4 ; i++){
-				if(went[now.x+direction[i][0]][now.y+direction[i][1]]==0)
-				if(mapp[now.x+direction[i][0]][now.y+direction[i][1]]=='.'
-				||mapp[now.x+direction[i][0]][now.y+direction[i][1]]=='T'){
-//					cout << "Add " << now.x+direction[i][0] << " " << now.y+direction[i][1] << endl;
+//				if(now.step==0 || now.stage[now.x+direction[i][0]][now.y+direction[i][1]]!='S')
 				
-//					cout << "--------------------------\n";
-//					for(int x = 0 ; x < MAPHEIGHT;x++){
-//						for(int j = 0 ; j < MAPLENGTH ; j++){
-//							cout << char(went[x][j]+'0');
-//						}
+				if(now.step==0 || (now.act[now.step-1] != zzz[i]))
+				if(now.stage[now.x+direction[i][0]][now.y+direction[i][1]]=='.'
+				||now.stage[now.x+direction[i][0]][now.y+direction[i][1]]=='T'
+				||now.stage[now.x+direction[i][0]][now.y+direction[i][1]]=='B'
+				||now.stage[now.x+direction[i][0]][now.y+direction[i][1]]=='S'){
+//					showWent();
+					
+//					showStage(now);
+					int Br,Bc;
+//					cout << "--------------------------showStage\n";
+					for(int z = 0 ; z < r;z++){
+						for(int j = 0 ; j < c ; j++){
+//							cout << now.stage[z][j];
+							if(now.stage[z][j]=='B')
+								Br=z,Bc=j;
+						}
 //						cout << endl;
-//					}
-//					cout << "--------------------------\n";	
+					}
+//					cout << "--------------------------\n";
+					
+					for(int z = 0 ; z < 4 ;z++){
+						if(now.stage[Br+direction[z][0]][Bc+direction[z][1]]=='#')
+						if(now.stage[Br+direction[(z+1)%4][0]][Bc+direction[(z+1)%4][1]]=='#')
+							break;
+							break;
+					}
+					
+					position temp=now;
+					temp.x = now.x+direction[i][0];
+					temp.y = now.y+direction[i][1];
+					
+					
+					if(now.stage[now.x+direction[i][0]][now.y+direction[i][1]]=='B'
+						&&(now.stage[now.x+2*direction[i][0]][now.y+2*direction[i][1]]=='.'
+						||now.stage[now.x+2*direction[i][0]][now.y+2*direction[i][1]]=='T')){
+						temp.act[temp.step++] = ccc[i+4];
+						temp.stage[now.x+direction[i][0]][now.y+direction[i][1]]='S';
+						temp.stage[now.x+2*direction[i][0]][now.y+2*direction[i][1]]='B';
+						temp.stage[now.x][now.y]='.';
+						que.push(temp);
+					}
+					else if (now.stage[now.x+direction[i][0]][now.y+direction[i][1]]=='.'){
+						temp.act[temp.step++] = ccc[i];
+						temp.stage[now.x][now.y]='.';
+						temp.stage[now.x+direction[i][0]][now.y+direction[i][1]]='S';
+						que.push(temp);
+					}
 				
-					if(mapp[now.x+direction[i][0]][now.y+direction[i][1]]=='T'){
+//					if(now.stage[now.x+direction[i][0]][now.y+direction[i][1]]=='T'){
+					if(now.stage[goal.x][goal.y]=='B'){
 						for(int i = 0 ; i <= now.step ; i++)
 							cout << now.act[i];
 						cout << "\nFOUND GOALLLL!!!!\n";
 						return 1;
 					}
-					else{
-//						cout << i << endl;
-						position temp=now;
-						temp.x = now.x+direction[i][0];
-						temp.y = now.y+direction[i][1];
-						temp.act[temp.step++] = ccc[i];
-						
-						que.push(temp);
-					}
+					
 					
 				}
 				
 			}
 		}
-	
 	} // end while(1)
 	
 	
 }
 
 /* MAPP:
+3 5
+#####
+#TBS#
+#####
+
+3 7
+#######
+#T..BS#
+#######
+
 5 5
 #####
 #...#
-#T.S#
+#TBS#
 #...#
 #####
 
+5 6
+######
+#....#
+#T.BS#
+#....#
+######
+
+5 6
+######
+#T#..#
+#..BS#
+#....#
+######
+
 7 11
 ###########
-#.##.....T#
+#T##......#
 #.#.#..####
 #....B...S#
 #.######..#
+#.........#
+###########
+
+7 11
+###########
+#.........#
+#..T...####
+#....B...S#
+#.........#
 #.........#
 ###########
 */
